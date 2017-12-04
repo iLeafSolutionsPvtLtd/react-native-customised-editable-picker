c7e4cfc45faf4b591dd59c348c2a2767dad9fc84
Ganga <ganga@ileafsolutions.net>
2017-12-04 16:17:18 +0530

    first commit

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-import { AppRegistry } from 'react-native';
-import React, { Component } from 'react';
 import CustomDropDown from './CustomDropDown';
 
-let data  = ["React","Native","Android","Java","Hello World"];
- class App extends Component{
-  render(){
-    return(
-      <CustomDropDown
-      data={data}
-      pickerStyle={this.props.pickerStyle}
-      modalStyle={this.props.modalStyle}
-      textinputStyle={this.props.textinputStyle}
-      isEditable={true}
-      />
-    )
-  }
-}
-
-AppRegistry.registerComponent('CustomDropDown', () => App);
+export default CustomDropDown;
